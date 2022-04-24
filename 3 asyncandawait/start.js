29dabf8de0c7d5d7d9ac491c6b81f171dedad59e
dearpollapat <70456916+dearpollapat@users.noreply.github.com>
2022-04-24 18:24:41 +0700

    Add files via upload

---
+++ 3 asyncandawait/start.js
@@ -0,0 +1,71 @@
+// How Asynchronus code works in JavaScript
+
+// function simulateAsyncAPI(text , timeout){
+//   setTimeout(() => console.log(text) , timeout);
+// }
+
+// simulateAsyncAPI('A',1000);
+// simulateAsyncAPI('B',500);
+// simulateAsyncAPI('C',100);
+//-----------------------------------------------------------------------//
+// Callback
+// function simulateAsyncAPI(text , timeout , callback){
+//   setTimeout(() => {
+//     console.log(text);
+
+//     if(callback){
+//       callback();
+//     }
+//   },timeout);
+// }
+
+// simulateAsyncAPI('A',1000,() =>{
+//   simulateAsyncAPI('B',500,() => {
+//     simulateAsyncAPI('C',100,() =>{})
+//   })
+// });
+
+// Promise
+// function simulateAsyncAPI(text,timeout){
+//   return new Promise((resolve , reject) =>{
+//     setTimeout(() => {
+//       if(text === 'B') return reject('B got rejected');
+//       console.log(text);
+//       resolve();
+//     }, timeout);
+//   })
+// }
+
+// simulateAsyncAPI('A',1000)
+// .then(() => {
+//   return simulateAsyncAPI('B',500);
+// })
+// .then(() => {
+//   return simulateAsyncAPI('C',100);
+// })
+// .catch((error) => {
+//   console.error(error);
+// })
+
+// Async/Await
+function simulateAsyncAPI(text, timeout) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      // if(text === 'B') return reject('B got rejected');
+      console.log(text);
+      resolve();
+    }, timeout);
+  });
+}
+
+async function run(){
+  try{
+    await simulateAsyncAPI('A',1000);
+    await simulateAsyncAPI('B',500);
+    await simulateAsyncAPI('C',100);
+
+  }catch(error){
+    console.error(error)
+  }
+}
+run();
